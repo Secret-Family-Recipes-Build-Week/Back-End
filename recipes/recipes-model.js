64c4a4fd835b4bdb54f4dae5b42e9e02b013191b
EmilyLow <emlow@me.com>
2020-11-13 16:06:30 -0600

    Get includes ingredients and instructions

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -2,13 +2,79 @@ const db = require("../database/config");
 
 module.exports = {getAllRecipes, findById, addRecipe, updateRecipe, removeRecipe};
 
-function getAllRecipes() {
-    return db('users');
+function getShortRecipe() {
+    return db('recipes');
 }
 
-function findById(id) {
+async function getAllRecipes() {
+    try{
+      let counted = await db("recipes").count("id");
+      let length = counted[0]['count(`id`)']
+      let recipeArray = [];
+      for(let i = 1; i <= length; i++) {
+        let recipe = await findById(i);
+        // console.log("recipe", recipe)
+        recipeArray.push(recipe)
+      }
+    //   console.log("ra", recipeArray);
+      return recipeArray;
 
+    } catch {
+        return "Something went wrong when compiling recipes"
+    }
+    // try {
+    //     return await db("recipes")
+    //         .innerJoin("ingredients", "ingredients.recipe_id", "recipes.id")
+    //         .innerJoin("instructions", "instructions.recipe_id", "recipes.id")
+    //         .select("recipes.*", "ingredients.*", "instructions.*" )
+    
+
+    // } 
+    // catch(err) {
+    //     console.log("Failure to get all recipes, err:", err)
+    //     throw "error code";
+    // }
+   //Promise.all()
+    
+    
+  
+
+
+
+//   temp.forEach(recipe => {
+//      let tempRecipe = findById(recipe.id)
+//      final.push({...recipe, tempRecipe})
+//   })
+
+//     return final;
+    // return db("recipes")
 }
+
+async function findById(id) {
+    
+    try {
+        let parentData = await db("recipes").where({id}).first();
+        let ingredientData = await db("ingredients").where("recipe_id", id).select("ingredient")
+        let instructionData = await db("instructions").where("recipe_id", id).select("instruction")
+        // console.log(instructionData)
+        // console.log(parentData)
+        // console.log(ingredientData)
+        parentData.ingredients = ingredientData
+        parentData.instructions = instructionData
+        console.log(parentData)
+        return parentData;
+        // return db("recipes").where({id}).first();
+    } catch(err) {
+        return "Something went wrong when compiling recipes"
+    }
+
+
+
+
+ return db("recipes").where({id}).first();
+    
+}
+
 
 function addRecipe(recipe) {
 
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -8,28 +8,52 @@ const router = express.Router();
 //!!May add next to all of these 
 
 //This will add ingredients and steps to the recipes, using model
-router.get('/recipes', (req, res) => {
+router.get('/recipes', async (req, res, next) => {
+    try {   
+        res.json(await Recipes.getAllRecipes())
+
+    } catch(err){
+        next(err)
+    }
 
 })
 
 //This will add ingredients and steps to the recipe, using model
-router.get('/recipes/:id', (req, res) => {
+router.get('/recipes/:id', async (req, res, next) => {
     const id = req.params.id;
+    try {
+        res.json(await Recipes.findById(id))
+    } catch(err){
+        next(err)
+    }
 })
 
 //This will edit ingredients and steps table as well as recipe table, using model
-router.put('/recipes/:id', (req,res) => {
+router.put('/recipes/:id', (req,res, next) => {
+    try {
+
+    } catch(err){
+        next(err)
+    }
 
 })
 
 //Will add recipes, steps, and ingredients all at once, using model
-router.post('/recipes', (req, res) => {
+router.post('/recipes', async (req, res) => {
+    try {
 
+    } catch(err){
+        next(err)
+    }
 })
 
 //Ensure deletion properly cascades
 router.delete('./recipes/:id', (req, res) => {
+    try {
 
+    } catch(err){
+        next(err)
+    }
 })
 
 module.exports = router;
